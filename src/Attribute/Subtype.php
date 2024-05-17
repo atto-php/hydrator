ac53102070ea3bfdc0a74721ef45b02b7f886f91
Carnage <t.carnage@gmail.com>
2024-05-17 18:46:13 +0100

    Tighten up attributes

--- src/Attribute/Subtype.php
+++ src/Attribute/Subtype.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Atto\Hydrator\Attribute;
 
-#[\Attribute()]
+#[\Attribute(\Attribute::TARGET_PROPERTY)]
 final class Subtype
 {
     public function __construct(
