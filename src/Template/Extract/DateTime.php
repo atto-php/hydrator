88e16014fff7535e7617c5ea45aa4021f8fdd576
Chris Riley <chris.riley@sportserve.co>
2024-12-29 16:54:19 +0000

    Add serialisation support for nest strategy

--- src/Template/Extract/DateTime.php
+++ src/Template/Extract/DateTime.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Atto\Hydrator\Template\Extract;
 
 use Atto\Hydrator\Attribute\SerializationStrategyType;
+use Atto\Hydrator\Template\ArrayReference;
 use Atto\Hydrator\Template\ObjectReference;
 
 final class DateTime
@@ -12,4 +13,21 @@ final class DateTime
     const EXTRACT_FORMAT = '%s->format(\DATE_ATOM)';
 
     use Basic;
+
+    private readonly ArrayReference $arrayReference;
+    private readonly ObjectReference $objectReference;
+
+    public function __construct(
+        private readonly string $propertyName,
+        private readonly ?SerializationStrategyType $serialisationStrategy,
+        private readonly bool $nullable,
+    ) {
+        $this->arrayReference = new ArrayReference($this->propertyName);
+        $this->objectReference = new ObjectReference($this->propertyName);
+    }
+
+    private function getExtractFormat(string $objectReference): string
+    {
+        return sprintf(self::EXTRACT_FORMAT, $objectReference);
+    }
 }
